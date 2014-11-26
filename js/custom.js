fa7a43fc260d39581253b87c00ea784104a009ac
caiyingying <caiyingying234@gmail.com>
2014-11-26 13:47:08 +0800

    表头样式的修改

--- js/custom.js
+++ js/custom.js
@@ -1,18 +1,18 @@
 $(document).ready(function(){
 	var curLocation = window.location.href;
-	if("http://daoliuhe.com" == curLocation){
+	if("http://daoliuhe.com/" == curLocation){
 		$("#main").addClass("active");
 		
-	}else if("http://daoliuhe.com/pages/categorys.html" == curLocation){
+	}else if(curLocation.indexOf("categorys.html") > 0 ){
 		$("#category").addClass("active");
 	
-	}else if("http://daoliuhe.com/pages/photo.html" == curLocation){
-			$("#photo").addClass("active");
+	}else if(curLocation.indexOf("photo.html") > 0 ){
+		$("#photo").addClass("active");
 		
-	}else if("http://daoliuhe.com/pages/message.html" == curLocation){
+	}else if(curLocation.indexOf("message.html") > 0 ){
 		$("#message").addClass("active");
 
-	}else if("http://daoliuhe.com/pages/about.html" == curLocation){
+	}else if(curLocation.indexOf("about.html") > 0 ){
 		$("#about").addClass("active");
 		
 	}
