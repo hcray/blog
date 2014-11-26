2fcf2e637c78caf1ab982e50f23de7f3b5143572
caiyingying <caiyingying234@gmail.com>
2014-11-26 13:32:49 +0800

    文件夹的重命名

--- js/custom.js
+++ js/custom.js
@@ -3,16 +3,16 @@ $(document).ready(function(){
 	if("http://daoliuhe.com" == curLocation){
 		$("#main").addClass("active");
 		
-	}else if("http://daoliuhe.com/categorys/categorys.html" == curLocation){
+	}else if("http://daoliuhe.com/pages/categorys.html" == curLocation){
 		$("#category").addClass("active");
 	
-	}else if("http://daoliuhe.com/categorys/phote.html" == curLocation){
+	}else if("http://daoliuhe.com/pages/phote.html" == curLocation){
 			$("#phote").addClass("active");
 		
-	}else if("http://daoliuhe.com/categorys/message.html" == curLocation){
+	}else if("http://daoliuhe.com/pages/message.html" == curLocation){
 		$("#message").addClass("active");
 
-	}else if("http://daoliuhe.com/categorys/about.html" == curLocation){
+	}else if("http://daoliuhe.com/pages/about.html" == curLocation){
 		$("#about").addClass("active");
 		
 	}
